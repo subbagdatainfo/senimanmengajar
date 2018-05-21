5860c03a37d2615700952ea185b5e335a92ffa90
abdulhadikaryana <karyana.abdhadi@gmail.com>
2018-05-21 22:28:24 +0700

    revisi persyaratan pendaftaran

--- application/views/persyaratan.php
+++ application/views/persyaratan.php
@@ -5,7 +5,9 @@
                     <div class="row-lg-6">
                         <h3 class="section-heading">Persyaratan Seniman</h3>
                         <p>
-                            Seniman dalam kegiatan Seniman Mengajar adalah individu ataupun kelompok (max 5 orang) yang memiliki latar belakang seni baik secara akademik maupun non-akademik baik yang telah memiliki pengalaman dalam dunia seni ataupun yang baru lulus, dengan kriteria sebagai berikut:
+                            Seniman dalam kegiatan Seniman Mengajar adalah individu ataupun kelompok (max 3 orang) yang memiliki latar belakang seni 
+                            baik secara akademik maupun non-akademik baik yang telah memiliki pengalaman dalam dunia seni ataupun yang baru lulus,
+                            dengan kriteria sebagai berikut:
                         </p>
                         <ul>
                             <li>1.  Non-Pegawai Negeri Sipil.</li>
@@ -13,19 +15,21 @@
                             <li>3.  Sehat jasmani dan rohani.</li>
                             <li>4.  Mampu beradaptasi dengan lingkungan penempatan.</li>
                             <li>5.  Aktif dalam kegiatan kesenian yang melibatkan komunitas/masyarakat</li>
-                            
+               
                         </ul>
                     </div>
                     <div class="row-lg-6">
                         <h3 class="section-heading">PROPOSAL KEGIATAN</h3>
                         <p>
-                            Proposal kegiatan adalah rancangan kerja dalam bentuk kegiatan yang dibuat oleh masing-masing peserta Seniman Mengajar selama 30 (tiga puluh) hari. Proposal digunakan sebagai acuan peserta untuk bekerja yang nantinya dapat disesuaikan secara teknis dengan kondisi lapangan pada saat orientasi lapangan berlangsung.
+                            Proposal kegiatan adalah rancangan kerja dalam bentuk kegiatan yang dibuat oleh masing-masing peserta 
+                            Seniman Mengajar selama 30 (tiga puluh) hari. Proposal digunakan sebagai acuan peserta untuk bekerja yang 
+                            nantinya dapat disesuaikan secara teknis dengan kondisi lapangan pada saat orientasi lapangan berlangsung. 
                             Proposal ajuan seniman dapat berupa:
 
                         </p>
                         <ul>
                             <li>1.  Program seni yang melibatkan komunitas dan/atau masyarakat local*</li>
-                            <li>2. Workshop seni kepada komunitas dan/atau masyarakat lokal</li>
+                            <li>2.  Workshop seni kepada komunitas dan/atau masyarakat lokal</li>
                             <li>3.  Karya yang berbasis kesenian tradisi dengan melibatkan komunitas dan/atau masyarakat lokal </li>
                             <li>4.  Karya riset yang berbasis kesenian tradisi dengan melibatkan komunitas dan/atau masyarakat lokal.</li>
                             
@@ -33,30 +37,61 @@
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <h3 class="section-heading">Kriteria proposal</h3>
-                    <p>
+                    <div class="row-lg-3">
+                        <h3 class="section-heading">Kriteria proposal</h3>
+                        <p>
+                            <ul>
+                                <li>
+                                    1.  Surat motivasi (1-2 hal A4 ukuran font 12), berupa tulisan singkat mengenai ketertarikan dan motivasi mengikuti program Seniman Mengajar
+                                </li>
+                                <li>
+                                    2.  Proposal pembelajaran sesuai dengan aspek penguatan karakter bangsa
+                                </li>
+                                <li>
+                                    3.  Memperhatikan budaya dan kondisi masyarakat tempat tujuan
+                                </li>
+                                <li>
+                                    4.  Mencantumkan target capaian 
+                                </li>
+                                <li>
+                                    5.  Mempertimbangkan keberlanjutan kegiatan
+                                </li>
+
+                            </ul>
+                        <p>
+                            Petunjuk teknis dan pedoman pendaftaran kegiatan Seniman Mengajar dapat diunduh
+                            <a href="<?php echo HTTP_ASSETS_PATH_ADMIN;?>petunjuk_teknis_senimanmengajar_2018.pdf">di sini</a>
+                        </p>
+                    </div>
+                    <div class="row-lg-3">
+                        <h3 class="section-heading">PENDAFTARAN PESEREORANGAN</h3>
+                        <p>Untuk pendaftaran perseorangan harus mencantumkan sebagai berikut:</p>
                         <ul>
-                            <li>
-                                1.  Surat motivasi (1-2 hal A4 ukuran font 12), berupa tulisan singkat mengenai ketertarikan dan motivasi mengikuti program Seniman Mengajar
-                            </li>
-                            <li>
-                                2.  Proposal pembelajaran sesuai dengan aspek penguatan karakter bangsa
-                            </li>
-                            <li>
-                                3.  Memperhatikan budaya dan kondisi masyarakat tempat tujuan
-                            </li>
-                            <li>
-                                4.  Mencantumkan target capaian 
-                            </li>
-                            <li>
-                                5.  Mempertimbangkan keberlanjutan kegiatan
-                            </li>
+                            <li>1. Peserta wajib membuat propsosal sesuai dengan ketentuan</li>
+                            <li>2. Mengirimkan 1(satu) file dengan format (PDF/.doc/.docx) </li>
                         </ul>
-                    <p>
-                        Petunjuk teknis dan pedoman pendaftaran kegiatan Seniman Mengajar dapat diunduh
-<a href="<?php echo HTTP_ASSETS_PATH_ADMIN;?>PETUNJUK TEKNIS DAN PEDOMAN PENDAFTARA SENIMAN MENGAJAR 2018.pdf">di sini</a>
-                    </p>
-                    
+                    </div>
+                    <div class="row-lg-3">
+                        <h3 class="class-section-heading">PENDAFTARAN KELOMPOK</h3>
+                        <p>Untuk pendaftaran kelompok harus mencantumkan sebagai berikut:</p>
+                        <ul>
+                            <li>1. Tiap anggota dalam kelompok wajib membuat proposal sesuai dengan ketentuan</li>
+                            <li>2. tujuan utama dalam proposal dapat dijadikan sama dengan anggota kelompok yang lain 
+                                dengan mempertimbangkan program kegiatan tiap individu dalam kelompok</li>
+                            <li>3. Mengirimkan 1(satu) file dengan format (PDF/.doc/.docx) bagi tiap anggota kelompok</li>
+                        </ul>
+                        <p>
+                            Contoh proposal pendaftaran dapat diunduh <a href="<?php echo HTTP_ASSETS_PATH_ADMIN;?>contoh_proposal.pdf">(di sini)</a>
+                        </p>
+                    </div>
+                    <div class="row-lg-3">
+                        <h3 class="section-heading">TATA CARA PENDAFTARAN</h3>
+                        <p>Pendaftaran Seniman Mengajar dapat dilakukan dengan 2(dua) cara yaitu:</p>
+                        <ul>
+                            <li>1. Melalui website senimanmengajar.kemdikbud.go.id</li>
+                            <li>2. Mengirimkan berkas proposal yang telah dikompilasi kedalam 1(satu) dokumen dengan file dengan format (PDF/.doc/.docx) ke alamat email senimanmengajar@kemdikbud.go.id</li>
+                        </ul>
+                    </div>                
                 </div>
             </div>
         </div>
