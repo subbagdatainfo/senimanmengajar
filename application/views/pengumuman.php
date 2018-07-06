7ca45df0341eb10a371517dad4676bf6264df373
abdulhadikaryana <karyana.abdhadi@gmail.com>
2018-07-06 10:24:33 +0700

    add pengumuman lolos wawancara

--- application/views/pengumuman.php
+++ application/views/pengumuman.php
@@ -3,12 +3,12 @@
             <div clas ="row">
                 <!-- <h1><span class="label label-success"> Pendaftaran sudah Ditutup</span><h1> -->
                 <div class ="col-lg-12 text-center">
-                    <p><h2 class="intro-text ">Pengumuman Lolos Seleksi Proposal<br>Terima Kasih Kepada yang Sudah Mendaftar. 
-                        <br>Surat Keputusan pendaftar yang lolos seleksi proposal dapat diunduh <a href="<?php echo HTTP_ASSETS_PATH_ADMIN;?>pengumuman_lolos_seleksi.pdf">Di Sini</a>
+                    <p><h2 class="intro-text ">Pengumuman Lolos Seleksi Wawanacara
+                        <br>Surat Keputusan pendaftar yang lolos seleksi wawancara dapat diunduh <a href="<?php echo HTTP_ASSETS_PATH_ADMIN;?>PENGUMUMAN HASIL SELEKSI WAWANCARA SENIMAN MENGAJAR 2018.pdf">Di Sini</a>
                         
                     </h2>
                     </p>
-                    <p><h3><br>Bagi peserta yang lolos seleksi akan dihubungi lebih lanjut oleh panitia untk teknis seleksi wawancara</h3></p>
+                    
                 </div>
             </div>
            
